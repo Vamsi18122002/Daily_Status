b8bde6d5eab1ca9f6cface9c14c496424a850cee
VamsiM <vamsimallu46@gmail.com>
2026-01-08 18:40:56 +0530

    08-01-2026

---
+++ 07-01-2026/Practice_types_of_CSS_07-01-2026.js
@@ -0,0 +1,4 @@
+console.log("types of CSS")
+console.log("Inline css")
+console.log("Internal css")
+console.log("external css")
---
+++ 08-01-2026/Practice&Revise_types_of_CSS_08-01-2026.js
@@ -0,0 +1,4 @@
+console.log("Types of CSS")
+console.log("Inline css -> Inline CSS is used to apply style directly inside an HTML element using the style attribute.")
+
+console.log("Internal css -> Internal CSS is written inside a <style> tag within the <head> section of an HTML page and is used to style a single web page.")
